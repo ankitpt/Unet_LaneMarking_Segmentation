7fc2ff1cdf5c96d29f85368604f0360d49711996
Ankit Patel <pate1019@purdue.edu>
2019-07-05 18:17:48 -0400

    Better lane width estimation through test cases

--- lane2.py
+++ lane2.py
@@ -6,13 +6,9 @@ start = time.time()
 import numpy as np
 import matplotlib.pyplot as plt
 import os
-from PIL import Image
 import cv2
 import math
 import shutil
-
-
-
 
 def get_dis(c1,c2,vx,vy):
     
@@ -90,7 +86,15 @@ except:
     pass
 os.mkdir('C:/Users/17657/Desktop/DPRG/try')
 
-for img_k in [4]:
+#for img_k in range(1,63):
+for img_k in [23]:
+    
+    [va1,vb1,a1,b1]=cv2.fitLine(np.array([[geos[img_k,4],geos[img_k,5]],[geos[img_k,10],geos[img_k,11]]]), cv2.DIST_L2,0,0.01,0.01)
+    [va2,vb2,a2,b2]=cv2.fitLine(np.array([[geos[img_k,6],geos[img_k,7]],[geos[img_k,8],geos[img_k,9]]]), cv2.DIST_L2,0,0.01,0.01)
+    c1=b1-(vb1*a1)/va1
+    c2=b2-(vb2*a2)/va2
+    
+    
     
     cl=None
     cr=None
@@ -124,14 +128,17 @@ for img_k in [4]:
         dx=vx/dist
         dy=vy/dist
 
-        x_right=x1+(pix_dis)*(dy)
-        x_left=x1-(pix_dis)*(dy)
+        x_right=int(x1+(pix_dis)*(dy))
+        x_left=int(x1-(pix_dis+6)*(dy))
         
-        y_right=y1-(pix_dis+12)*(dx)
-        y_left=y1+(pix_dis+3)*(dx)
-        xnew=np.arange(int(np.round(x_left)),int(np.round(x_right))+1,1)
-        nimg[y1,xnew]=img[y1,xnew]
-    
+        y_right=int(y1-(pix_dis)*(dx))
+        y_left=int(y1+(pix_dis+6)*(dx))
+        #xnew=np.arange(int(np.round(x_left)),int(np.round(x_right))+1,1)
+        #nimg[y_left:y_right,x_left:x_right]=img[y1,xnew]
+        if(y_right==y_left):
+            nimg[y_left,x_left:x_right]=img[y_left,x_left:x_right]
+        else:
+            nimg[y_right:y_left,x_left:x_right]=img[y_right:y_left,x_left:x_right]
     
     #cv2.line(img_trj,(y1,x1),(y2,x2),(255,0,0),2)
     
@@ -149,7 +156,23 @@ for img_k in [4]:
     
     
     if(len(contours)<=1):
+        os.chdir("C:/Users/17657/Desktop/DPRG/lw")
         print("Wide lane marking detected on only one side in image ",img_k)
+        
+        c_trj=y-(vy*x)/vx
+        det=-(vb1/va1)+vy/vx
+        y1=int((-(vb1/va1)*c_trj+(vy/vx)*c1)/det)
+        y2=int((-(vb2/va2)*c_trj+(vy/vx)*c2)/det)
+        ys=np.linspace(y1,y2,50)
+        for y_temp in ys:
+                
+                x_trj=(y_temp-(y-(vy/vx)*x))*(vx/vy)
+                X=geos[img_k,0]+0.05*sfs[img_k,0]*x_trj
+                Y=geos[img_k,1]+0.05*sfs[img_k,1]*(y_temp)
+                print( 20,img_k,X[0],Y,file=open("lw_"+tile+".txt", "a"))
+
+            
+        os.chdir('C:/Users/17657/Desktop/DPRG')
         wide.append(img_k)
         continue
     
@@ -166,54 +189,127 @@ for img_k in [4]:
     left_sign=np.sign(255-(vy/vx)*0-c_cept)
     right_sign=np.sign(0-(vy/vx)*255-c_cept)
     
+    pot=[]
     lftm_mag=0
     rtm_mag=0
+    l_flag=0
+    r_flag=0
+    l_thresh=10
+    r_thresh=10
+    l_theta_min=90
+    r_theta_min=90
+    angl_thresh=7
+    angr_thresh=7
     #finding left and rightmost lane marker
-    for contour in contours:
+    #for contour in contours:
+    while(k<len(contours)):
         
-        A=cv2.contourArea(contour)
-        P=cv2.arcLength(contour,True)+0.000001
+        
+        
+        [vx_temp,vy_temp,x_temp,y_temp]=cv2.fitLine(contours[k], cv2.DIST_L2,0,0.01,0.01)
+        
+        theta=np.arccos(np.clip(vx*vx_temp+vy*vy_temp, a_min = -1, a_max = 1))*(180/np.pi)
+        
+        if(theta>90):
+            theta=180-theta
+
+        if(theta>10):
+            
+            k=k+1
+            continue
+        print(theta,k)
+        
+        if(k==0):
+            
+            lftm_mag=0
+            rtm_mag=0
+            pot=[]
+            
+        #checking side of contour
+        mean=np.mean(contours[k], axis=0)
+        sign_check=mean[0][1]-(vy/vx)*mean[0][0]-c_cept
+       # print(k,sign_check)
+        
+        
+        A=cv2.contourArea(contours[k])
+        P=cv2.arcLength(contours[k],True)+0.000001
         
         if(round(A)==0 or round(P)==0 or A==None or P==None):
+        
             k=k+1
             continue
-
         
-        W=sfs[img_k,0]*(P-np.sqrt(P*P-16*A))/4
-        L=sfs[img_k,1]*(P+np.sqrt(P*P-16*A))/4
-        
-        hull = cv2.convexHull(contour)
-        hull_area = cv2.contourArea(hull)
-        solidity = float(A)/hull_area
-        
-       #checking A/P ratio to see if the cluster is invalid
-        if(math.isnan(W) or math.isnan(L) or (W<2 or L<20) or (L<30 and solidity<0.75) ):
-            k=k+1
-            #print(cv2.contourArea(contour)/cv2.arcLength(contour,True))
-            continue
-        
-       
+        W=(P-np.sqrt(P*P-16*A))/4
+        L=(P+np.sqrt(P*P-16*A))/4
+       # print("before",L,W)
+        W=np.sqrt((W*abs(vy)*sfs[img_k,0])**2+(W*abs(vx)**sfs[img_k,1])**2)
+        L=np.sqrt((L*abs(vx)*sfs[img_k,0])**2+(L*abs(vy)**sfs[img_k,1])**2)
+       # print("after",L,W)
+      
+        if((l_flag==0 or r_flag==0) and L>10):
             
-        mean=np.mean(contour, axis=0)
-  #      leftmost = tuple(contour[contour[:,:,0].argmin()][0])
-   #     rightmost = tuple(contour[contour[:,:,0].argmax()][0])
-    #    topmost = tuple(contour[contour[:,:,1].argmin()][0])
-    
-        sign_check=mean[0][1]-(vy/vx)*mean[0][0]-c_cept
+            pot.append(k)
+       #checking A/P ratio to see if the cluster is invalid
+        elif(L>15 or theta<6):
+           pot.append(k)
+       
+       
+        if(np.sign(sign_check)==left_sign):
+            
+            if(math.isnan(W) or math.isnan(L) or (W<2 or L<l_thresh) ):
+                
+                k=k+1
+            #print(cv2.contourArea(contour)/cv2.arcLength(contour,True))
+                continue
+        else:
+            
+            if(math.isnan(W) or math.isnan(L) or (W<2 or L<r_thresh) ):
+                
+                k=k+1
+            #print(cv2.contourArea(contour)/cv2.arcLength(contour,True))
+                continue
+
+
         #if(leftmost[0]<lftm ):
-        if(np.sign(sign_check)==left_sign and abs(sign_check)>lftm_mag):
-        #if(mean[0][0]<lftm and np.sign(mean[0][1]-(vy/vx)*mean[0][0]-c_cept)==left_sign):   
-            cl=k
+        if(np.sign(sign_check)==left_sign):
+            #pot.append(k)
+            
+            if(L>25 and l_flag==0):
+                    
+                    l_thresh=25
+                    k=0
+                    l_flag=1
+                    angl_thresh=5
+                    pot=[]
+                    continue
+          
+            if(abs(sign_check)>lftm_mag and theta<4):  
+                cl=k
             #lftm=leftmost[0]
-            lftm=mean[0][0]
-            lftm_mag=abs(sign_check)
+                lftm=mean[0][0]
+                lftm_mag=abs(sign_check)
+                l_theta_min=theta
     #        lftm_y=mean[0][1]
         #if(rightmost[0]>rtm):
         #elif(mean[0][0]>rtm and np.sign(mean[0][1]-(vy/vx)*mean[0][0]-c_cept)==right_sign):
-        elif(np.sign(sign_check)==right_sign and abs(sign_check)>rtm_mag):    
-            cr=k
-            rtm=mean[0][0]
-            rtm_mag=abs(sign_check)
+        elif(np.sign(sign_check)==right_sign):
+           # pot.append(k)
+            
+                
+            if(L>20 and r_flag==0):
+                    
+                    r_thresh=20
+                    k=0
+                    r_flag=1
+                    angr_thresh=5
+                    pot=[]
+                    continue
+                
+            if(abs(sign_check)>rtm_mag and theta<5):
+                cr=k
+                rtm=mean[0][0]
+                rtm_mag=abs(sign_check)
+                r_theta_min=theta
      #       rtm_y=mean[0][1]
         
         k=k+1    
@@ -226,59 +322,37 @@ for img_k in [4]:
     except:
         
         print("Wide lane since markings on right or left side of proper dimensions could not be detected in ",img_k)
+        os.chdir("C:/Users/17657/Desktop/DPRG/lw")
+        c_trj=y-(vy*x)/vx
+        det=-(vb1/va1)+vy/vx
+        y1=int((-(vb1/va1)*c_trj+(vy/vx)*c1)/det)
+        y2=int((-(vb2/va2)*c_trj+(vy/vx)*c2)/det)
+        ys=np.linspace(y1,y2,50)
+        
+        for y_temp in ys:
+                
+                x_trj=(y_temp-(y-(vy/vx)*x))*(vx/vy)
+                X=geos[img_k,0]+0.05*sfs[img_k,0]*x_trj
+                Y=geos[img_k,1]+0.05*sfs[img_k,1]*(y_temp)
+                print( 20,img_k,X[0],Y,file=open("lw_"+tile+".txt", "a"))
         wide.append(img_k)
+        os.chdir("C:/Users/17657/Desktop/DPRG")
         continue
         
     [vx_lref,vy_lref,x_lref,y_lref] = cv2.fitLine(contours[cl], cv2.DIST_L2,0,0.01,0.01)
     [vx_rref,vy_rref,x_rref,y_rref] = cv2.fitLine(contours[cr], cv2.DIST_L2,0,0.01,0.01)
     #Finding right and left grouping of clusters
-    cleft=list()
-    cright=list()
+    cleft=[cl]
+    cright=[cr]
     
     
-    for contour in contours:
-        
-        A=cv2.contourArea(contour)
-        P=cv2.arcLength(contour,True)+0.000001
-        
-        if(round(A)==0 or round(P)==0 or A==None or P==None):
-            k=k+1
+#    for contour in contours:
+    for k in pot:       
+            
+        if(k==cl or k==cr):
             continue
-        
-        W=sfs[img_k,0]*(P-np.sqrt(P*P-16*A))/4
-        L=sfs[img_k,1]*(P+np.sqrt(P*P-16*A))/4
-        
-        hull = cv2.convexHull(contour)
-        hull_area = cv2.contourArea(hull)
-        solidity = float(A)/hull_area
-        
-       #checking A/P ratio to see if the cluster is invalid
-        if(math.isnan(W) or math.isnan(L) or (W<2 or L<20) or (L<30 and solidity<0.75)):            
-            k=k+1
-            continue
-        
-        if(np.all(contour==contours[cl]) or np.all(contour==contours[cr])):
             
-            if(k==cl):
-                
-                cleft.append(k)
-            else:
-                cright.append(k)
-                
-            
-            k=k+1
-            #[vx,vy,x,y] = cv2.fitLine(contour, cv2.DIST_L2,0,0.01,0.01)
-           # pro_pnts=project(contour,vx,vy,x,y)
-            #for pnt in pro_pnts:
-                
-             #   cv2.circle(rgb,(int(round(pnt[0])),int(round(pnt[1]))), 0, (0,255,0), -1)
-            
-            
-            continue
-        
-        
-        #
-        cmean=np.mean(contour, axis=0)
+        cmean=np.mean(contours[k], axis=0)
         
         vl=(cmean-lmean).flatten()
         vr=(cmean-rmean).flatten()
@@ -308,7 +382,7 @@ for img_k in [4]:
         if(rangle>90):
             rangle=180-rangle
         
-        if(langle<5):
+        if(langle<angl_thresh):
             
             cleft.append(k)
             print("The contour ",k," belongs to left side",langle)
@@ -318,7 +392,7 @@ for img_k in [4]:
                 
             #    cv2.circle(rgb,(int(round(pnt[0])),int(round(pnt[1]))), 0, (0,255,0), -1)
     
-        elif(rangle<5):
+        elif(rangle<angr_thresh):
             
             cright.append(k)
             print("The contour ",k," belongs to right side",rangle)
@@ -332,7 +406,7 @@ for img_k in [4]:
             
             
             os.chdir("C:/Users/17657/Desktop/DPRG/ambiguous")
-            tmean=np.mean(contour, axis=0)
+            tmean=np.mean(contours[k], axis=0)
             print( tmean[0],img_k,file=open("ambiguous_"+tile+".txt", "a"))
             print("The contour ",k," is ambiguous",langle,rangle)
             #emp.append(img_k)
@@ -363,10 +437,6 @@ for img_k in [4]:
             
             rp.append(pnt)
     
-    #if(len(lp) == 0 or len(rp)==0):
-        
-     #   print("Lane in image ",img_k," is too wide to detect markings on either left or right side")
-      #  continue
         
     [vxl,vyl,xl,yl] = cv2.fitLine(np.array(lp), cv2.DIST_L2,0,0.01,0.01)   
     [vxr,vyr,xr,yr] = cv2.fitLine(np.array(rp), cv2.DIST_L2,0,0.01,0.01)   
@@ -386,11 +456,7 @@ for img_k in [4]:
     
     os.chdir("C:/Users/17657/Desktop/DPRG/lw")
     
-    
-    [va1,vb1,a1,b1]=cv2.fitLine(np.array([[geos[img_k,4],geos[img_k,5]],[geos[img_k,10],geos[img_k,11]]]), cv2.DIST_L2,0,0.01,0.01)
-    [va2,vb2,a2,b2]=cv2.fitLine(np.array([[geos[img_k,6],geos[img_k,7]],[geos[img_k,8],geos[img_k,9]]]), cv2.DIST_L2,0,0.01,0.01)
-    c1=b1-(vb1*a1)/va1
-    c2=b2-(vb2*a2)/va2
+
     cl_2=yl-(vyl*xl)/vxl
     
     det=-(vb1/va1)+vyl/vxl
@@ -400,22 +466,26 @@ for img_k in [4]:
     
     
     ys=np.linspace(y1,y2,50)
+
     
-    for y in ys:
+    for y_temp in ys:
         
-        x_lt=(y-(yl-(vyl/vxl)*xl))*(vxl/vyl)
+        x_lt=(y_temp-(yl-(vyl/vxl)*xl))*(vxl/vyl)
+        x_trj=(y_temp-(y-(vy/vx)*x))*(vx/vy)
         
-        X=geos[img_k,0]+0.05*sfs[img_k,0]*x_lt
-        Y=geos[img_k,1]+0.05*sfs[img_k,1]*y
+        X=geos[img_k,0]+0.05*sfs[img_k,0]*x_trj
+
+        Y=geos[img_k,1]+0.05*sfs[img_k,1]*y_temp
+    
         a=vyr
         b=-vxr
         c=vxr*yr-vyr*xr
         #lane_width=(abs(a*x_lt+b*y+c)/np.sqrt(a*a+b*b))*0.05*(sfs[img_k,0])*3.28084
-        pix_lane_width=(abs(a*x_lt+b*y+c)/np.sqrt(a*a+b*b))
+        pix_lane_width=(abs(a*x_lt+b*y_temp+c)/np.sqrt(a*a+b*b))
         x_dis=pix_lane_width*abs(vyr)*0.05*sfs[img_k,0]
         y_dis=pix_lane_width*abs(vxr)*0.05*sfs[img_k,1]
         lane_width=np.sqrt(x_dis*x_dis+y_dis*y_dis)*3.28
-        print("Estimated lane width is, ",lane_width," feet")
+        print("Estimated lane width is, ",lane_width,img_k," feet")
         print( lane_width[0],img_k,X[0],Y,file=open("lw_"+tile+".txt", "a"))
     
     os.chdir("C:/Users/17657/Desktop/DPRG")
